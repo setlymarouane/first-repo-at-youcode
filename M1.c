54bf33733e61c41990499911db3ad8ce3b69273d
Oughlane <oughlanemohammed72@gmail.com>
2025-09-02 15:38:36 +0200

    'ma.c'

---
+++ M1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float M,MA;
+    printf("ENTRE la montant \n");
+    scanf("%f",&M);
+    MA=M*1.05;
+    printf("Le montant avec interet est: %.2f\n",MA);
+    return 0;
+}
---
+++ m0.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+    float r,v;
+    
+    printf("ENTRE LA RAYON \n");
+    scanf("%f",&r);
+    v=(4/3)*3.14*r*r*r;
+    printf("Le volume de la sphere est: %.1f\n",v);
+    return 0;
+}
