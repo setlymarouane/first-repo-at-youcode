9483f2152976cfb3967a048660ff5bf9674c737e
Oughlane <oughlanemohammed72@gmail.com>
2025-09-10 14:50:19 +0200

    ex1

--- m2.c
+++ m2.c
@@ -1,10 +1,19 @@
 #include<stdio.h>
 int main (){
-    //ex5
-    int x,p;
-    printf("ENTRE un nombre \n");
-    scanf("%d",&x);
-    p=3*x*x*x*x*x+2*x*x*x*x-5*x*x*x-x*x+7*x-6;
-    printf("Le polynome est: %d\n",p);
-return 0;
+    int GS1,GS2,GS3,GS4,GS5;
+    printf("ENTRE l'ISBN \n");
+    do{
+         printf("entrer gsi? : ");
+         scanf("%d",&GS1);
+    }while(GS1!=978 && GS1!=979);
+    if(GS1!=978 && GS1!=979){
+        scanf("%d %d %d %d %d",&GS1,&GS2,&GS3,&GS4,&GS5);
+    printf("Prefix GS1: %d\n",GS1);
+    printf("Identifient de groupe %d\n",GS2);
+    printf("code de l'editeur %d\n",GS3);
+    printf("numero d'article %d\n",GS4);
+    printf("chiffre de controle %d\n",GS5);
+    }
+   
+    return 0;
 }
